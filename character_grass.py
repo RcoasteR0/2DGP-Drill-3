44963e0b8c8be736877d8448fd6b9d4e0056fc72
RcoasteR0 <st_anthony@naver.com>
2024-09-24 12:28:40 +0900

    사각운동 기본구조

--- character_grass.py
+++ character_grass.py
@@ -6,29 +6,46 @@ open_canvas()
 grass = load_image('grass.png')
 boy = load_image('character.png')
 
+def run_top():
+   print('top')
+   pass
+
+def run_right():
+   print('right')
+   pass
+
+def run_bottom():
+   print('bottom')
+   pass
+
+def run_left():
+   print('left')
+   pass
+
 def run_rectangle():
    print('Rectangle')
+   run_top()
+   run_right()
+   run_bottom()
+   run_left()
    pass
 
 def run_circle():
    print('Circle')
+   r, cx, cy = 300, 800 // 2, 600 // 2
+   for degree in range(0, 360, 3):
+      theta = math.radians(degree)
+      x = r * math.cos(theta) + cx
+      y = r * math.sin(theta) + cy
+
+      clear_canvas_now()
+      boy.draw_now(x, y)
+      delay(0.1)
    pass #내용이 없는 빈 함수
-
-r, cx, cy = 300, 800 // 2, 600 // 2
-
-for degree in range(0, 360, 3):
-
-    theta = math.radians(degree)
-    x = r * math.cos(theta) + cx
-    y = r * math.sin(theta) + cy
-
-    clear_canvas_now()
-    boy.draw_now(x, y)
-    delay(0.1)
 
 while True:
    run_rectangle()
-   run_circle()
+   #run_circle()
    break
 
 close_canvas()
