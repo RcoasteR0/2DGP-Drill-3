13558abbed87cc2ababcda52d33fa62460eac618
RcoasteR0 <st_anthony@naver.com>
2024-09-24 12:33:56 +0900

    윗줄 이동 완성

--- character_grass.py
+++ character_grass.py
@@ -6,8 +6,15 @@ open_canvas()
 grass = load_image('grass.png')
 boy = load_image('character.png')
 
+def draw_boy(x, y):
+    clear_canvas_now()
+    boy.draw_now(x, y)
+    delay(0.1)
+
 def run_top():
    print('top')
+   for x in range(0, 800, 10):
+      draw_boy(x, 550)
    pass
 
 def run_right():
@@ -34,18 +41,16 @@ def run_circle():
    print('Circle')
    r, cx, cy = 300, 800 // 2, 600 // 2
    for degree in range(0, 360, 3):
-      theta = math.radians(degree)
-      x = r * math.cos(theta) + cx
-      y = r * math.sin(theta) + cy
+        theta = math.radians(degree)
+        x = r * math.cos(theta) + cx
+        y = r * math.sin(theta) + cy
 
-      clear_canvas_now()
-      boy.draw_now(x, y)
-      delay(0.1)
+        draw_boy(x, y)
    pass #내용이 없는 빈 함수
 
 while True:
    run_rectangle()
-   #run_circle()
+   run_circle()
    break
 
 close_canvas()
