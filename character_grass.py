589dbbb4bed3d67866d7a03e5abcd3819171ac3a
RcoasteR0 <st_anthony@naver.com>
2024-09-24 11:57:03 +0900

    캐릭터 화면 출력

--- character_grass.py
+++ character_grass.py
@@ -3,7 +3,7 @@ from pico2d import *
 open_canvas()
 
 grass = load_image('grass.png')
-character = load_image('character.png')
+boy = load_image('character.png')
 
 def run_rectangle():
    print('Rectangle')
@@ -13,8 +13,13 @@ def run_circle():
    print('Circle')
    pass #내용이 없는 빈 함수
 
+clear_canvas_now()
+boy.draw_now(400, 300)
+delay(1)
+
 while True:
    run_rectangle()
-   run_circle() 
+   run_circle()
+   break
 
 close_canvas()
