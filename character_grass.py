d5c36596938dc325cd6b8d0174ab600cdd368f65
RcoasteR0 <st_anthony@naver.com>
2024-09-24 12:12:15 +0900

    원 운동 구현

--- character_grass.py
+++ character_grass.py
@@ -1,4 +1,5 @@
 from pico2d import *
+import math
 
 open_canvas()
 
@@ -13,9 +14,17 @@ def run_circle():
    print('Circle')
    pass #내용이 없는 빈 함수
 
-clear_canvas_now()
-boy.draw_now(400, 300)
-delay(1)
+r, cx, cy = 300, 800 // 2, 600 // 2
+
+for degree in range(0, 360, 3):
+
+    theta = math.radians(degree)
+    x = r * math.cos(theta) + cx
+    y = r * math.sin(theta) + cy
+
+    clear_canvas_now()
+    boy.draw_now(x, y)
+    delay(0.1)
 
 while True:
    run_rectangle()
