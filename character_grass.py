37dbc7b86d2ed1daa6973f108f0c9efbfbd7328e
RcoasteR0 <st_anthony@naver.com>
2024-09-24 11:49:57 +0900

    무한루프 구조 확인

--- character_grass.py
+++ character_grass.py
@@ -2,6 +2,19 @@ from pico2d import *
 
 open_canvas()
 
-# fill here
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+def run_rectangle():
+   print('Rectangle')
+   pass
+
+def run_circle():
+   print('Circle')
+   pass #내용이 없는 빈 함수
+
+while True:
+   run_rectangle()
+   run_circle() 
 
 close_canvas()
